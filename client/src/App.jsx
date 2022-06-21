4ea9e8fff089053cc6c48ee23c5b359a68526464
Charles Stevens <cs-interface@protonmail.ch>
2022-06-21 20:30:28 +0000

    conditional rendering is the way

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
 import React from 'react'
-import {Link} from 'react-router-dom'
 import $ from 'jquery'
-import Edit from './Edit'
 
 class App extends React.Component {
   constructor(props) {
@@ -26,7 +24,14 @@ class App extends React.Component {
     }
   }
   delet = (e) => {
+    $.ajax({
+      type: 'DELETE',
+      url: `http://localhost:3001/todos/${e.target.id}`
+    })
     this.getInfo()
+  }
+  edit = () => {
+    console.log("Edit clicked")
   }
   render() {
     if (this.state.info) {
@@ -44,10 +49,8 @@ class App extends React.Component {
           {
             this.state.info.map(todo => <div className="todo" key={todo.id}>
               {todo.data}
-              <svg id={todo.id} className="btn" onclick={this.delet} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
-              <Link to="#" element={<Edit/>}/>
-              <svg id={todo.id} className="btn" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
-              </Link>
+              <svg id={todo.id} className="btn" onClick={this.delet} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
+              <svg id={todo.id} className="btn" onClick={this.edit} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
             </div>)
           }
         </div>
--- client/src/Edit.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class Edit extends React.Component {
-  render() {
-    return <div>Insert edit page here</div>
-  }
-}
-
-export default Edit
